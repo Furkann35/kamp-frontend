fef097706b50f533b3df180d3b219e60a15577b6
Furkann35 <yucelfurkan124@gmail.com>
2021-05-31 18:53:32 +0300

    31.05.2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'northwind';
+
+export class AppCoponent {
+  title: string = 'northwind';
+  user: string = "Furkan Yücel";
 }
