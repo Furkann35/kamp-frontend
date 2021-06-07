775d8959f513230bb313d6a27225a5c57f50dfc4
Furkann35 <yucelfurkan124@gmail.com>
2021-06-08 00:07:14 +0300

    Sepete ekleme,sepet silme,toastr service yazıldı.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,9 @@
 import { stringify } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -14,7 +16,7 @@ export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
   filterText="";
-  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) { }
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute,private toastrService:ToastrService,private cartService:CartService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params=>{
@@ -39,5 +41,10 @@ export class ProductComponent implements OnInit {
       this.products = response.data
       this.dataLoaded = true;
     })
+  }
+
+  addToCart(product:Product){
+   this.toastrService.success("Sepete eklendi",product.productName )
+   this.cartService.addToCart(product)
   }
 }
